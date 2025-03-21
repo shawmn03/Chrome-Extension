f3c9f0c34468907595b9cd9ca61e87f559cee0cc
Megan Shaw <shawmn@vcu.edu>
2025-03-21 11:15:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ function replaceText(element) {
     element.childNodes.forEach(replaceText);
   } else if (element.nodeType === Text.TEXT_NODE) {
     if (element.textContent.match(/coronavirus/gi)) {
-      element.parentElement.style.color = 'black';
-      element.parentElement.style.backgroundColor = 'black';
+      const newElement = document.createElement('span');
+      newElement.innerHTML = element.textContent.replace(/(coronavirus)/gi, '<span style="background-color: black; color: black;">$1</span>');
+      element.replaceWith(newElement);
     }
   }
 }
