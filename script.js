92fbcfebcd71410e611c6957576d3fa31428b490
Megan Shaw <shawmn@vcu.edu>
2025-03-21 11:14:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 alert('Hello!');
+
+function replaceText(element) {
+  if (element.hasChildNodes()) {
+    element.childNodes.forEach(replaceText);
+  } else if (element.nodeType === Text.TEXT_NODE) {
+     element.textContent = element.textContent.replace(/coronavirus/gi, '■■■■■■■■■■■');
+  }
+}
+
+replaceText(document.body);
